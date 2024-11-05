cfe555f943d5e3ff396613c8a07aadbeb15498b9
Abdifatah Ibrahim <cfataxyare5716@gmail.com>
2024-11-05 23:01:05 +0300

    Updated

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ app.get('/posts', async (req, res) => {
   }
 });
 
+app.get('/posts/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    const result = await pool.query('SELECT title, content FROM posts WHERE id =$1', [id]);
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Database error:', error.message);
+    res.status(500).send('Error retrieving posts');
+  }
+});
 
 // Start the server
 const PORT = process.env.PORT || 3000;
